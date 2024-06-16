3e12e5d0e58e28d72e3ae2d1217a0f8104eb11c7
Damian Kreft <25139693+damiankreft@users.noreply.github.com>
2024-06-16 21:53:18 +0200

    Hilt setup v1 - snapshot before just before I start messing with frameworks' versions.

---
+++ app/src/main/java/pl/edu/pja/teamk/timetracking/HiltApplication.kt
@@ -0,0 +1,8 @@
+package pl.edu.pja.teamk.timetracking
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+}
--- app/src/main/java/pl/edu/pja/teamk/timetracking/ui/home/HomeViewModel.kt
+++ app/src/main/java/pl/edu/pja/teamk/timetracking/ui/home/HomeViewModel.kt
@@ -1,13 +1,22 @@
 package pl.edu.pja.teamk.timetracking.ui.home
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import pl.edu.pja.teamk.timetracking.TimeEntryStore
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor(handle: SavedStateHandle, private val store: TimeEntryStore) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _testText = MutableLiveData<String>().apply {
+        value = store.testFun()
     }
-    val text: LiveData<String> = _text
+    var testText: MutableLiveData<String> = _testText
 }
+
+
+//@HiltViewModel
+//class DonutViewModel @Inject constructor(handle: SavedStateHandle?, repository: RecipeRepository?) :
+//    ViewModel()
