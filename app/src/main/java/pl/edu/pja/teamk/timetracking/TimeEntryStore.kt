f0d968dc8f9443f2bd3fef6d252e9e225568ab88
Damian Kreft <25139693+damiankreft@users.noreply.github.com>
2024-06-17 03:05:25 +0200

    (ATT-5) Add initial implementation of app's persistent handling.

--- app/src/main/java/pl/edu/pja/teamk/timetracking/TimeEntryStore.kt
+++ app/src/main/java/pl/edu/pja/teamk/timetracking/TimeEntryStore.kt
@@ -1,8 +1,41 @@
 package pl.edu.pja.teamk.timetracking
 
-class TimeEntryStore() {
+import android.content.Context
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
+import java.util.Date
+
+class TimeEntryStore() : TimeSource {
     init { }
+    var data: MutableList<DayData> = mutableListOf()
+
     fun testFun(): String {
         return "Hilt works!"
     }
+
+    override fun loadData(context: Context) {
+        val json = Json { ignoreUnknownKeys = true }
+        val filename = "appdata"
+        val file = File(context.filesDir, filename)
+        val text = file.readText()
+        // handle exceptions
+
+        data = json.decodeFromString<MutableList<DayData>>(text)
+    }
+
+    override fun saveData(context: Context, data: MutableList<DayData>) {
+        val json = Json { ignoreUnknownKeys = true }
+        val dataJson = json.encodeToString(data)
+        val filename = "appdata"
+        val file = File(context.filesDir, filename)
+        file.writeText(dataJson)
+
+        // handle exceptions
+    }
+}
+
+interface TimeSource {
+    fun loadData(context: Context)
+    fun saveData(context: Context, data: MutableList<DayData>)
 }
