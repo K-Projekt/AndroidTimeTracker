21e8a51e95649dbfe32bab30bc419c5c33a7534a
Damian Kreft <25139693+damiankreft@users.noreply.github.com>
2024-06-21 04:00:52 +0200

    (ATT-5) Open entrie's details page on click.

--- app/src/main/java/pl/edu/pja/teamk/timetracking/ui/TimeEntriesFragment.kt
+++ app/src/main/java/pl/edu/pja/teamk/timetracking/ui/TimeEntriesFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.commit
 import dagger.hilt.android.AndroidEntryPoint
 import pl.edu.pja.teamk.timetracking.R
+import pl.edu.pja.teamk.timetracking.TimeEntry
 import pl.edu.pja.teamk.timetracking.databinding.FragmentTimeEntriesBinding
 import pl.edu.pja.teamk.timetracking.ui.home.HomeViewModel
 import pl.edu.pja.teamk.timetracking.ui.home.areDateEqual
@@ -39,10 +41,19 @@ class TimeEntriesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentTimeEntriesBinding.bind(view)
         binding.list.adapter = MyTimeEntryRecyclerViewAdapter(mutableListOf())
-        (binding.list.adapter as MyTimeEntryRecyclerViewAdapter).setData(viewModel.storeData.data.filter { areDateEqual(it.start, viewModel.selectedDate) }.toMutableList())
+        val adapter = binding.list.adapter as MyTimeEntryRecyclerViewAdapter
+        adapter.onClickListener = View.OnClickListener {
+            val item = it.tag as TimeEntry
+            val fragmentManager = parentFragmentManager
+            fragmentManager.commit {
+                replace(R.id.fragment_home_container, TimeEntryDetails())
+                addToBackStack(null)
+            }
+        }
+        adapter.setData(viewModel.storeData.data.filter { areDateEqual(it.start, viewModel.selectedDate) }.toMutableList())
         viewModel.listObservers.add {selected ->
             val data = viewModel.storeData.data.filter { areDateEqual(it.start, selected) }.toMutableList()
-            (binding.list.adapter as MyTimeEntryRecyclerViewAdapter).setData(data)
+            adapter.setData(data)
         }
     }
 
